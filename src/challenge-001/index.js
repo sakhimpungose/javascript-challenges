cc6907eb4a05552637fef0856148d060f264bb9a
Sakhile Mpungose <sakhi.mpungose365@gmail.com>
2023-10-02 01:22:00 +0200

    Added test for challange-001

---
+++ src/challenge-001/index.js
@@ -0,0 +1,6 @@
+function encrypt(key, message)
+{
+    
+}
+
+module.exports = encrypt;
---
+++ src/challenge-001/index.test.js
@@ -0,0 +1,39 @@
+const chai = require('chai');
+
+const encrypt = require('./index');
+
+describe('encrypt', function()
+{
+    it('Test Case 1: should return corrrect encrypted message', function()
+    {
+        const key = 'ab c';
+        const message = 'abc ab';
+        const expected = 'ba cba';
+
+        const encryptedMessage = encrypt(key, message);
+
+        chai.expect(encryptedMessage).to.equal(expected);
+    });
+
+    it('Test Case 2: should return corrrect encrypted message', function()
+    {
+        const key = 'otorhinolaryngological';
+        const message = 'My name is Paul';
+        const expected = 'Mr olme hs Plua';
+
+        const encryptedMessage = encrypt(key, message);
+
+        chai.expect(encryptedMessage).to.equal(expected);
+    });
+
+    it('Test Case 3: should return corrrect encrypted message', function()
+    {
+        const key = 'gaderypoluk';
+        const message = 'This is an encrypted message';
+        const expected = 'Thks ks gn dncyrotde mdssgad';
+
+        const encryptedMessage = encrypt(key, message);
+
+        chai.expect(encryptedMessage).to.equal(expected);
+    });
+});
