38dc1a4d5b7195a40c0f796f9264e6e952f91128
Sakhile Mpungose <sakhi.mpungose365@gmail.com>
2023-10-02 02:09:14 +0200

    added encrypt

--- src/challenge-001/index.js
+++ src/challenge-001/index.js
@@ -1,6 +1,17 @@
 function encrypt(key, message)
 {
-    
+    return message
+        .split('')
+        .map(function(ch)
+        {
+            const i = key.indexOf(ch);
+            if(i === -1)
+            {
+                return ch;
+            }
+            return (i + 1) % 2 === 0 ? key[i - 1] : key[i + 1];
+        })
+        .join('');
 }
 
 module.exports = encrypt;
